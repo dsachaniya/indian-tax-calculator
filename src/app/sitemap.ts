e503373321ce09d807ba290bfc9d395f8c1cd914
darshansachaniya <dms94260@gmail.com>
2025-09-04 12:45:33 +0530

    fixed prod build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://taxgenius.in'
+  const baseUrl = 'https://dsachaniya.github.io/indian-tax-calculator'
   
   return [
     {
@@ -9,6 +11,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/gst-section-14-time-of-supply`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     // Add more pages here as you expand the application
     // {
